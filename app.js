fbd7b92cec52b032621a0ea764b56085379d8b55
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2023-05-07 20:52:32 -0700

    finally got the credentials for mongoose DB. I used Mongosh shell too create a user with the correct auth. The Mongoose DB connects to the server.

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
-const express = require('express')
+require('dotenv').config()
+const express = require('express');
 // const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 // const methodOverride = require('method-override')
 const api = require('./api');
-
+const {MONGOOSE_USERNAME, MONGOOSE_PASSWORD, MONGOOSE_DB_NAME} = process.env;
 const app = express()
 const port = 3000
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+  authSource: 'admin',
+  user: MONGOOSE_USERNAME,
+  pass: MONGOOSE_PASSWORD
+  
+};
+const dbURL = `mongodb://localhost:27017/${MONGOOSE_DB_NAME}`;
+mongoose.connect(dbURL, dbOptions).then(() => {
+  console.log('Connected to MongoDB database');
+}).catch(err => {
+  console.error('Error connecting to MongoDB database', err);
+});
+
+// const businessSchema = new mongoose.Schema({
+//   ownerid: { required: true },
+//   name: { required: true },
+//   address: { required: true },
+//   city: { required: true },
+//   state: { required: true },
+//   zip: { required: true },
+//   phone: { required: true },
+//   category: { required: true },
+//   subcategory: { required: true },
+//   website: { required: false },
+//   email: { required: false }
+
+// });
+// const photoSchema = new mongoose.Schema({
+//   userid: { required: true },
+//   businessid: { required: true },
+//   caption: { required: false }
+// });
+// const reviewSchema = new mongoose.Schema({
+//   userid: { required: true },
+//   businessid: { required: true },
+//   rating: { required: true },
+//   review: { required: false }
+
+// });
 
 app.use(morgan('dev'));
 
