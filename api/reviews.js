c2e5943379879b4af82bb542fed00166819bc1b4
lucasrouchy <lucasrouchy1@gmail.com>
2023-05-11 19:24:35 -0700

    Finished the reviews API endpoints. ALl data is on my Mongo database. Moving on to docker.

--- api/reviews.js
+++ api/reviews.js
@@ -1,91 +1,71 @@
+const mongoose = require('mongoose');
+const express = require('express');
 const router = require('express').Router();
-const { validateAgainstSchema, extractValidFields } = require('../val/validation');
+// const { validateAgainstSchema, extractValidFields } = require('../val/validation');
 const reviewSchema = require('../models/review');
-const reviews = require('../db/reviews');
+// const reviews = require('../db/reviews');
 
 exports.router = router;
-exports.reviews = reviews;
+// exports.reviews = reviews;
 
-
-router.post('/', function (req, res, next) {
-    if (validateAgainstSchema(req.body, reviewSchema)) {
-      const review = extractValidFields(req.body, reviewSchema);  
-      const userReviewedThisBusinessAlready = reviews.some(
-        existingReview => existingReview
-          && existingReview.ownerid === review.ownerid
-          && existingReview.businessid === review.businessid
-      );
-  
-      if (userReviewedThisBusinessAlready) {
-        res.status(403).json({
-          error: "User has already posted a review of this business"
-        });
-      } else {
-        review.id = reviews.length;
-        reviews.push(review);
-        res.status(201).json({
-          id: review.id,
-          links: {
-            review: `/reviews/${review.id}`,
-            business: `/businesses/${review.businessid}`
-          }
-        });
-      }
-  
-    } else {
-      res.status(400).json({
-        error: "Request body is not a valid review object"
-      });
-    }
-});
-router.get('/:reviewID', function (req, res, next) {
-    const reviewID = parseInt(req.params.reviewID);
-    if (reviews[reviewID]) {
-      res.status(200).json(reviews[reviewID]);
-    } else {
-      next();
-    }
+router.post('/post', async (req, res) => {
+  const review = new reviewSchema({
+    _id: new mongoose.Types.ObjectId(),
+    id: req.body.id,
+    userid: req.body.userid,
+    businessid: req.body.businessid,
+    dollars: req.body.dollars,
+    stars: req.body.stars,
+    review: req.body.review
+  });
+  try{
+    const savedReview = review.save();
+    res.status(200).json(savedReview);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 });
 
-router.put('/:reviewID', function (req, res, next) {
-    const reviewID = parseInt(req.params.reviewID);
-    if (reviews[reviewID]) {
-  
-      if (validateAgainstSchema(req.body, reviewSchema)) {
-       
-        let updatedReview = extractValidFields(req.body, reviewSchema);
-        let existingReview = reviews[reviewID];
-        if (updatedReview.businessid === existingReview.businessid && updatedReview.userid === existingReview.userid) {
-          reviews[reviewID] = updatedReview;
-          reviews[reviewID].id = reviewID;
-          res.status(200).json({
-            links: {
-              review: `/reviews/${reviewID}`,
-              business: `/businesses/${updatedReview.businessid}`
-            }
-          });
-        } else {
-          res.status(403).json({
-            error: "Updated review cannot modify businessid or userid"
-          });
-        }
-      } else {
-        res.status(400).json({
-          error: "Request body is not a valid review object"
-        });
-      }
-  
-    } else {
-      next();
-    }
+router.get('/', async (req, res) => {
+  try{
+    const data = await reviewSchema.find();
+    res.json(data)
+  }
+  catch(error){
+      res.status(500).json({message: error.message})
+  }
 });
 
-router.delete('/:reviewID', function (req, res, next) {
-    const reviewID = parseInt(req.params.reviewID);
-    if (reviews[reviewID]) {
-      reviews[reviewID] = null;
-      res.status(204).end();
-    } else {
-      next();
-    }
+router.get('/:id', async (req, res) => {
+  try{
+    const review = await reviewSchema.findById(req.params.id);
+    res.json(review)
+  }
+  catch(error){
+      res.status(500).json({message: error.message})
+  }
+});
+router.patch('/update/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+    const result = await reviewSchema.findByIdAndUpdate(
+        id, updatedData, options
+    )
+    res.send(result)
+  }
+  catch (error) {
+      res.status(400).json({ message: error.message })
+  }
+});
+router.delete('/delete/:id', async(req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await reviewSchema.findByIdAndDelete(id)
+    res.send(`Document with ${data.businessid} has been deleted..`)
+  }
+  catch (error) {
+      res.status(400).json({ message: error.message })
+  }
 });
--- models/review.js
+++ models/review.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const reviewSchema = new mongoose.Schema({
+    id: { type: Number, required: true },
     userid: { type: Number,required: true },
     businessid: { type: Number,required: true },
-    rating: { type: Number,required: true },
+    dollars: { type: Number,required: true },
+    stars: { type: Number,required: true },
     review: { type: String,required: false }
   
 });
